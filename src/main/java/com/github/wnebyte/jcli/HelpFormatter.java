5a94db811dfe4a803a69b7d42f000ca6381e451e
wnebyte <emilssonpost@gmail.com>
2022-03-10 18:17:26 +0100

    commented out some currently unused code, and modified a test class to test the changes

--- src/main/java/com/github/wnebyte/jcli/HelpFormatter.java
+++ src/main/java/com/github/wnebyte/jcli/HelpFormatter.java
@@ -1,69 +1,98 @@
 package com.github.wnebyte.jcli;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
-import java.util.List;
 import java.util.function.Function;
 import com.github.wnebyte.jarguments.Argument;
+import com.github.wnebyte.jarguments.util.Chars;
+import com.github.wnebyte.jarguments.util.Strings;
+import com.github.wnebyte.jarguments.util.Objects;
 
 public class HelpFormatter implements Formatter<BaseCommand> {
 
-    private static final Formatter<BaseCommand> CMD_TO_STRING_FUNCT = BaseCommand::toPaddedDescriptiveString;
+    /*
+    ###########################
+    #     UTILITY METHODS     #
+    ###########################
+    */
 
-    private static final Formatter<Argument> ARG_TO_STRING_FUNCT = Argument::toString;
+    private static int maxLength(Collection<Argument> c) {
+        return c.stream().map(ARGUMENT_FORMATTER)
+                .max(Comparator.comparingInt(String::length))
+                .orElse(Strings.EMPTY)
+                .length();
+    }
+
+    private static int maxTotalLength(Collection<Argument> c) {
+        int max = maxLength(c);
+
+        return c.stream().map(new Function<Argument, String>() {
+            @Override
+            public String apply(Argument argument) {
+                String s = ARGUMENT_FORMATTER.apply(argument);
+                String indent = indent((max + 1) - s.length());
+                StringBuilder out = new StringBuilder();
+                out.append(Strings.WHITESPACE).append(s).append(indent)
+                        .append(Objects.requireNonNullElseGet(argument.getDesc(), () -> Strings.EMPTY));
+                return out.toString();
+            }
+        }).max(Comparator.comparingInt(String::length))
+                .orElse(Strings.EMPTY)
+                .length();
+    }
+
+    private static String indent(int len) {
+        char[] arr = new char[len];
+        Arrays.fill(arr, Chars.WHITESPACE);
+        return new String(arr);
+    }
+
+    /*
+    ###########################
+    #      STATIC FIELDS      #
+    ###########################
+    */
+
+    private static final Formatter<BaseCommand> COMMAND_FORMATTER = BaseCommand::toPaddedDescriptiveString;
+
+    private static final Formatter<Argument> ARGUMENT_FORMATTER = Argument::toPaddedString;
+
+    /*
+    ###########################
+    #         METHODS         #
+    ###########################
+    */
 
     @Override
     public String apply(BaseCommand cmd) {
         StringBuilder out = new StringBuilder();
-        out.append(cmd.hasDesc() ? cmd.getDesc().concat("\n") : "")
-                .append("Usage: ").append(CMD_TO_STRING_FUNCT.apply(cmd));
+        out.append(cmd.hasDesc() ? cmd.getDesc().concat("\n") : Strings.EMPTY)
+                .append("Usage: ").append(COMMAND_FORMATTER.apply(cmd));
 
         if (!cmd.getArguments().isEmpty()) {
-            int maxLength  = maxToStringLength(cmd.getArguments());
-            int maxTotalLength = maxTotalLength(cmd.getArguments());
-            out.append("\n\n").append("OPTIONS: ").append("\n");
+            int maxLength  = maxLength(cmd.getArguments());
+           // int maxTotalLength = maxTotalLength(cmd.getArguments());
+            out.append("\n\n")
+                    .append("OPTIONS: ")
+                    .append("\n");
 
-            for (Argument arg : cmd.getArguments()) {
-                String s = ARG_TO_STRING_FUNCT.apply(arg);
+            for (Argument argument : cmd.getArguments()) {
+                String s = ARGUMENT_FORMATTER.apply(argument);
                 String indent = indent((maxLength + 1) - s.length());
                 String ln = new StringBuilder().append("\t").append(s).append(indent)
-                        .append(arg.hasDesc() ? arg.getDesc() : "").toString();
+                        .append(Objects.requireNonNullElseGet(argument.getDesc(), () -> Strings.EMPTY)).toString();
                 out.append(ln);
+                /*
                 indent = indent((maxTotalLength + 1) - ln.length());
                 out.append(indent).append("\n");
-               // out.append(indent).append("<").append(arg.getType().getSimpleName()).append(">").append("\n");
+                 */
+                out.append("\n");
             }
         } else {
             out.append("\n");
         }
 
         return out.toString();
-    }
-
-    private int maxToStringLength(final List<Argument> args) {
-        return args.stream().map(ARG_TO_STRING_FUNCT).max(Comparator.comparingInt(String::length))
-                .orElse("")
-                .length();
-    }
-
-    private int maxTotalLength(final List<Argument> args) {
-        int max = maxToStringLength(args);
-
-        return args.stream().map(new Function<Argument, String>() {
-            @Override
-            public String apply(Argument arg) {
-                String s = ARG_TO_STRING_FUNCT.apply(arg);
-                String indent = indent((max + 1) - s.length());
-                StringBuilder out = new StringBuilder();
-                out.append(" ").append(s).append(indent).append(arg.hasDesc() ? arg.getDesc() : "");
-                return out.toString();
-            }
-        }).max(Comparator.comparingInt(String::length)).orElse("").length();
-    }
-
-    private String indent(int len) {
-        char[] arr = new char[len];
-        Arrays.fill(arr, ' ');
-        return new String(arr);
     }
 }
--- src/test/java/com/github/wnebyte/jcli/sample/SampleTest.java
+++ src/test/java/com/github/wnebyte/jcli/sample/SampleTest.java
@@ -1,7 +1,8 @@
 package com.github.wnebyte.jcli.sample;
 
-import com.github.wnebyte.jcli.annotation.Command;
 import com.github.wnebyte.jcli.CLI;
+import com.github.wnebyte.jcli.annotation.Argument;
+import com.github.wnebyte.jcli.annotation.Command;
 import com.github.wnebyte.jcli.conf.Configuration;
 
 public class SampleTest {
@@ -11,17 +12,23 @@ public class SampleTest {
                 .setScanClasses(FooController.class, BarController.class, SampleTest.class)
                 .nullifyScanPackages()
         );
-        cli.getConfiguration().out().println("--start--");
+        cli.getConfiguration().out().println("--begin");
         cli.read();
     }
 
-    @Command
+    @Command(description = "this is a description")
     public static void dab(int a) {
 
     }
 
-    @Command
-    public static void eab() {
+    @Command(description = "another description")
+    public static void eab(
+            @Argument(name = "looongname", description = "argument description")
+            String a,
+            @Argument(description = "argument description")
+            String b,
+            String c
+    ) {
 
     }
 
